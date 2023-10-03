13da037ebdda31c926c62a804f646ffab637c806
TakeshiB21075 <e1b21075@oit.ac.jp>
2023-10-03 14:51:56 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/z1814/z1976/z2133/team2/calculation/CalculationApplication.java
@@ -0,0 +1,13 @@
+package oit.is.z1814.z1976.z2133.team2.calculation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/z1814/z1976/z2133/team2/calculation/CalculationApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.z1814.z1976.z2133.team2.calculation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
